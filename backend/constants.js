741069038f8592fd58a8192542ac4489a28c43fd
Eduardo Da Silva <alu0101104911@ull.edu.es>
2023-06-26 19:52:24 +0100

    Update constants.js

--- backend/constants.js
+++ backend/constants.js
@@ -1,7 +1,8 @@
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 const DEEPL_APIKEY = "b9735e22-0655-c703-e64b-62c08bfbe09f:fx";
-const GENERAL_PATH = "C://Users//EduardoSY//Desktop//TFG//Desarrollo_TFG//MiTFG//TFG//backend";
+//const GENERAL_PATH = "C://Users//EduardoSY//Desktop//TFG//Desarrollo_TFG//MiTFG//TFG//backend";
+const GENERAL_PATH = "~";
 const UPLOADS_PATH = "/uploads";
 
 module.exports = {
