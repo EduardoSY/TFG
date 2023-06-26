bf4f96946ef7c429df1e2bb8b5c563c7b69acf90
Eduardo Da Silva <alu0101104911@ull.edu.es>
2023-06-26 20:55:57 +0100

    Limpiando código y formateando

--- backend/controllers/translation.js
+++ backend/controllers/translation.js
@@ -5,67 +5,66 @@ const path = require("path");
 const { URLSearchParams } = require("url");
 const { url } = require("inspector");
 const { resourceLimits } = require("worker_threads");
-const {DEEPL_APIKEY, UPLOADS_PATH} = require("../constants");
+const { DEEPL_APIKEY, UPLOADS_PATH } = require("../constants");
 
-const deepl = require('deepl-node');
+const deepl = require("deepl-node");
 const translator = new deepl.Translator(DEEPL_APIKEY);
 
+async function getTranslationLanguages(req, res) {
+  const response = await axios.get(
+    "https://api-free.deepl.com/v2/languages?type=target",
+    {
+      headers: {
+        Authorization: "DeepL-Auth-Key b9735e22-0655-c703-e64b-62c08bfbe09f:fx",
+      },
+    }
+  );
+  const resultado = JSON.stringify(response.data);
 
-async function getTranslationLanguages (req, res) {
-    const response = await axios.get('https://api-free.deepl.com/v2/languages?type=target', {
-        headers: {
-          'Authorization': 'DeepL-Auth-Key b9735e22-0655-c703-e64b-62c08bfbe09f:fx'
-        },
-      });
-      const resultado = JSON.stringify(response.data);
-
-      if(!response) {
-        //resultado = JSON.stringify(response.data);
-
-        //console.log("---######--------------------------------------------");
-        //console.log(response.data);
-        //console.log(JSON.stringify(response.data));
-  
-        res.status(400).send({msg: `Error al recibir de la API de DEEPL`});
-      } else{
-        //console.log(JSON.stringify(response.data));
-        res.status(200).send(JSON.stringify(response.data))
-      }
-};
-
-async function requestTranslation(req, res){
-    console.log("HE RECIBIDO UNA REQUEST PARA TRADUCIR");
-    const { token, selectedLanguage} = req.body;
-    //const token = "8bd2c4e2-f12e-4d08-9e2f-7175892787c7"
-    console.log(token);
-    console.log(selectedLanguage);
-
-    const vttFile = UPLOADS_PATH + `/${token}_original.vtt`;
-    console.log(vttFile);
-    const vttContent = fs.readFileSync(vttFile, 'utf8');
-    console.log(vttContent)
-    const translationResult = await translator.translateText(vttContent, null, selectedLanguage);
-    console.log(translationResult.text); // 'Bonjour, le monde !'
-
-    const translatedText = translationResult.text; // Texto traducido
-
-// Ruta y nombre del archivo de salida
-const translatedVttFile = UPLOADS_PATH + `/${token}_${selectedLanguage}.vtt`;
-
-fs.writeFile(translatedVttFile, translatedText, 'utf8', (err) => {
-  if (err) {
-    console.error('Error al guardar el archivo traducido:', err);
-    res.status(400).send({msg: `Error al guardar el archivo traducido`});
-    return;
+  if (!response) {
+    res.status(400).send({ msg: `Error al recibir de la API de DEEPL` });
+  } else {
+    res.status(200).send(JSON.stringify(response.data));
   }
+}
 
-  console.log('Archivo traducido guardado exitosamente:', translatedVttFile);
-  res.status(200);
-});
-    
+async function requestTranslation(req, res) {
+  const { token, selectedLanguage } = req.body;
+
+  //const vttFile = UPLOADS_PATH + `/${token}_original.vtt`;
+  const vttFile = path.join(__dirname, "../uploads", `${token}_original.vtt`);
+
+  const vttContent = fs.readFileSync(vttFile, "utf8");
+
+  const translationResult = await translator.translateText(
+    vttContent,
+    null,
+    selectedLanguage
+  );
+
+  const translatedText = translationResult.text;
+
+  // Ruta y nombre del archivo de salida
+  //const translatedVttFile = UPLOADS_PATH + `/${token}_${selectedLanguage}.vtt`;
+  const translatedVttFile = path.join(
+    __dirname,
+    "../uploads",
+    `${token}_${selectedLanguage}.vtt`
+  );
+
+  fs.writeFile(translatedVttFile, translatedText, "utf8", (err) => {
+    if (err) {
+      console.error("Error al guardar el archivo traducido:", err);
+      res.status(400).send({ msg: `Error al guardar el archivo traducido` });
+      return;
+    }
+
+    console.log("Archivo traducido guardado exitosamente:", translatedVttFile);
+    res.status(200);
+  });
 }
 
 module.exports = {
-    getTranslationLanguages,
-    requestTranslation
-  };
+  getTranslationLanguages,
+  requestTranslation,
+};
--- backend/controllers/vtt_files.js
+++ backend/controllers/vtt_files.js
@@ -5,102 +5,101 @@ const path = require("path");
 const { URLSearchParams } = require("url");
 const { url } = require("inspector");
 const { resourceLimits } = require("worker_threads");
-const {UPLOADS_PATH, GENERAL_PATH} = require("../constants");
+const { UPLOADS_PATH, GENERAL_PATH } = require("../constants");
 const { uptime } = require("process");
 
+/**
+ * Obtiene los ficheros de transcripción en funcion al id suministrado
+ *
+ * @param {number} id - El primer número.
+ * @returns {json} La suma de los dos números.
+ */
+async function get_transcription_files(req, res) {
+  const { id } = req.params;
+  const transcriptionId = id;
+  const directoryPath = path.join(__dirname, "../uploads");
+  console.log(directoryPath);
+  let debug_count = 0;
+  // Verifica si el directorio existe
+  if (fs.existsSync(directoryPath)) {
+    // Lee los archivos en el directorio
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ error: "Error al leer los archivos de transcripción" });
+        console.log("marico");
+      } else {
+        // Crea un array para almacenar la información de cada archivo
+        const regex = /_(.*?)\.vtt/;
+        const fileData = [];
+        // Itera sobre los archivos
+        files.forEach((file) => {
+          const filePath = path.join(directoryPath, file);
+          const match = regex.exec(file);
 
-async function get_transcription_files (req, res) {
-    //const transcriptionId = req.params.id;
-    const {id} = req.params;
-    const transcriptionId = id;
-    // Directorio donde se almacenan los archivos de transcripción
-    //const directoryPath = path.join(__dirname, 'uploads', transcriptionId);
-    //const directoryPath = GENERAL_PATH + UPLOADS_PATH;
-    const directoryPath = path.join(__dirname, '../uploads');
-    
-    console.log(directoryPath);
-    let debug_count = 0;
-  
-    // Verifica si el directorio existe
-    if (fs.existsSync(directoryPath)) {
-      // Lee los archivos en el directorio 
-      fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al leer los archivos de transcripción' });
-          console.log("marico");
-        } else {
-          // Crea un arreglo para almacenar la información de cada archivo
-          
-          const regex = /_(.*?)\.vtt/;
-          
-          const fileData = [];
-  
-          // Itera sobre los archivos y obtén la información relevante
-          files.forEach((file) => {
-            const filePath = path.join(directoryPath, file);
-            const fileStats = fs.statSync(filePath);
-            const match = regex.exec(file);
-          
-            if (file.includes(transcriptionId) && file.endsWith('.vtt')){
-                fileData.push({
-                    filename: file,
-                    filepath: filePath,
-                    language: match[1]
-                  });
-                 debug_count = debug_count + 1;
-            }
+          if (file.includes(transcriptionId) && file.endsWith(".vtt")) {
+            fileData.push({
+              filename: file,
+              filepath: filePath,
+              language: match[1],
+            });
+            debug_count = debug_count + 1;
+          }
+        });
 
-
-            // Agrega la información del archivo al arreglo
-           
-          });
-  
-          // Devuelve la información de los archivos como respuesta
-          res.json({ files: fileData });
-          console.log(debug_count);
-        }
-      });
-    } else {
-      // El directorio no existe, devuelve un error 404
-      res.status(404).json({ error: 'Transcripción no encontrada' });
-      console.log("asd"); 
-    }
-  };
-
-
-async function getVTTFiles(req, res) {
-    //const { active } = req.query;
-    let id = req.id;
-    response = get_transcription_files(id);
-    //let response = null;
-    //res.status(200).send(response);
-
-    if(!response) {
-        res.status(400).send({msg: `No se ha encontrado asdasd`});
-      } else{
-        console.log(response);
-        res.status(200).send(response)
+        // Devuelve la información de los archivos como respuesta
+        res.json({ files: fileData });
+        console.log(debug_count);
       }
+    });
+  } else {
+    // El directorio no existe, devuelve un error 404
+    res.status(404).json({ error: "Transcripción no encontrada" });
+    console.log("asd");
   }
+}
 
+/**
+ * Funcion utilizada en el endpoint
+ *
+ * @param {number} id
+ * @returns {response}
+ */
+async function getVTTFiles(req, res) {
+  let id = req.id;
+  response = get_transcription_files(id);
 
-function downloadVTTFile(req, res){
-  const {file} = req.params;
+  if (!response) {
+    res.status(400).send({ msg: `No se ha encontrado asdasd` });
+  } else {
+    console.log(response);
+    res.status(200).send(response);
+  }
+}
+
+/**
+ * Permite descargar un fichero VTT
+ *
+ * @param {number} file
+ * @returns {file}
+ */
+function downloadVTTFile(req, res) {
+  const { file } = req.params;
   console.log(file);
-  const vttFilePath = GENERAL_PATH + "/" + UPLOADS_PATH + "/" + file;
+  const vttFilePath = path.join(__dirname, "../uploads", file);
 
   res.download(vttFilePath, file, (err) => {
     if (err) {
       // Manejo del error
       console.error(err);
-      res.status(500).send('Error al descargar el archivo');
+      res.status(500).send("Error al descargar el archivo");
     }
   });
-  //res.status(200).send({msg: "Correcto hasta aqui"});
-
+  res.status(200).send("Fichero descargado correctamente");
 }
 
 module.exports = {
-    get_transcription_files,
-    downloadVTTFile
+  get_transcription_files,
+  downloadVTTFile,
 };
